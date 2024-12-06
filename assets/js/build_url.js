bd8f0ae96ca0caf37953bbee7c179dc639dcc0b9
nihongpaobing <74593871+nihongpaobing@users.noreply.github.com>
2024-12-06 13:57:10 +0800

    Update build_url.js

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -4,7 +4,7 @@ function build_url(){
         document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
     }
     else {
-        url = "https://nihongpaobing.github.io/qqwxredirect/api/?url=" + url;
+        url = "https://zz33.1916sport.com/api/?url=" + url;
         document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
     }
 }
